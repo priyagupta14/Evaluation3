390a33d85b62912eee9c59fa852f241c0c1c86e8
Priya Gupta <pgupta000205@gmail.com>
2021-03-15 17:34:12 +0530

    test: test for All Songs page

--- src/pages/AllSongs/AllSongs.test.jsx
+++ src/pages/AllSongs/AllSongs.test.jsx
@@ -4,18 +4,40 @@ import { BrowserRouter } from 'react-router-dom';
 import AllSongs from './AllSongs';
 
 describe(AllSongs.name, () => {
+  let mockProps;
   beforeAll(() => {
+    mockProps = {
+      allSongs: [{
+        id: '1234',
+        name: 'abc',
+        albumArtUrl: 'abc',
+        genre: {
+          name: 'abc',
+        },
+        artist: {
+          name: 'abc',
+        },
 
+        status: {
+          data: {
+            like: true,
+            count: 5,
+          },
+        },
+      },
+      ],
+      updateLikeState: jest.fn(),
+    };
   });
   test('should match snapshot', () => {
     const { container } = render(
-      <BrowserRouter><AllSongs /></BrowserRouter>
+      <BrowserRouter><AllSongs {...mockProps} /></BrowserRouter>
       ,
     );
     expect(container).toMatchSnapshot();
   });
   test('should display "all Songs"', () => {
-    render(<BrowserRouter><AllSongs /></BrowserRouter>);
-    screen.getByText('all Songs');
+    render(<BrowserRouter><AllSongs {...mockProps} /></BrowserRouter>);
+    screen.getByText('all songs');
   });
 });

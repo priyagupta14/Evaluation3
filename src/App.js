a60ded51d2192c8c526937621280d99eaf754e35
Priya Gupta <pgupta000205@gmail.com>
2021-03-15 13:26:54 +0530

    refactor: change normal app function into arrow function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import AllSongs from './pages/AllSongs/AllSongs';
 import LandingPage from './pages/LandingPage/LandingPage';
+import apiUtils from './utils/api/api';
 
-function App() {
+const App = () => {
+  const [allSongs, setAllSongs] = useState([]);
+
+  const getAllSongs = async () => {
+    const songs = await apiUtils.getAllSongs();
+    setAllSongs(songs.data);
+    console.log(songs.data);
+  };
   return (
     <div className="App">
       <NavBar />
       <Switch>
         <Route exact path="/">
-          <LandingPage />
+          <LandingPage getAllSongs={getAllSongs} />
+        </Route>
+        <Route path="/allSongs">
+          <AllSongs allSongs={allSongs} />
         </Route>
       </Switch>
     </div>
   );
-}
+};
 
 export default App;

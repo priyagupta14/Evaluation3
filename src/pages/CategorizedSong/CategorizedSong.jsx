b8b68fb3630952813013ce13d166715890d623eb
Priya Gupta <pgupta000205@gmail.com>
2021-03-15 15:17:28 +0530

    feat: add styling to the components/pages

--- src/pages/CategorizedSong/CategorizedSong.jsx
+++ src/pages/CategorizedSong/CategorizedSong.jsx
@@ -1,15 +1,27 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { Link } from 'react-router-dom';
+import allSongs from '../../assets/images/icon-grid.svg';
 import Song from '../../components/Song/Song';
+import './CategorizedSong.scss';
 
 const CategorizedSong = ({ songs }) => {
   console.log(songs);
   return (
-    <div>
+    <div className="categorizedSong">
+      <header>
+        <h1>genres</h1>
+        <Link to="/allSongs">
+          <button type="button">
+            <img src={allSongs} alt="" />
+          </button>
+        </Link>
+
+      </header>
       {Object.keys(songs).map((eachSong) => (
         <div key={eachSong}>
           <h1>{eachSong}</h1>
-          <div className="idk">
+          <div className="all-song-cards">
             {songs[eachSong].map((song, index) => (
               <Song
                 key={song.id}
